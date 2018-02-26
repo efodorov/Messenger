5d68043d74f205a187335b789451e638f2de3cc7
efodorov <>
2018-02-26 15:56:08 +0100

    init message

--- messenger/src/main/java/org/ef/javabrains/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/ef/javabrains/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package org.ef.javabrains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,25 +16,44 @@ import org.ef.javabrains.messenger.model.Message;
 import org.ef.javabrains.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	//create a new instance of messageService
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		//returning a call to a service
 		return messageService.getAllMessages();
 	}
 	
-	//here can be another method, e.g. POST method
-	//@POST
+
+	@POST
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long mid, Message message) {
+		message.setId(mid);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long mid) {
+		messageService.deleteMessage(mid);
+	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long mid) {
 		return messageService.getMessage(mid);
 	}
+	
+	
 }
